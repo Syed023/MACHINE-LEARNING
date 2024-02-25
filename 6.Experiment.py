1f99d994b84e46d33d26a9fb6b10070a89da9ce3
Syed023 <152625426+Syed023@users.noreply.github.com>
2024-02-25 13:31:59 +0530

    6.Experiment.py

--- 6.Experiment.py
+++ 6.Experiment.py
@@ -4,21 +4,10 @@ import pandas as pd
 dataset = pd.read_csv("C:\ML program\breastcancer (1).csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-"""
-Splitting the dataset into the Training set and Test set
-Once the dataset is read into the memory, next, divide the dataset into two parts, training and 
-testing using the train_test_split function from sklearn.
- The test_size and random_state attributes are set to 0.25 and 0 respectively. 
- You can change these attributes as per your requirements.
-"""
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-#Feature Scaling
-"""
-Feature scaling is the process of converting the data into a min-max range. In this case,
- the standard scalar method is used.
-"""
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
